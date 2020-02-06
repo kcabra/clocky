cd63574c1fee01553e554e6ac43b69d22c88e63e
kcabra <kcabrak@gmail.com>
2020-02-06 02:31:27 -0300

    playing with shaders

--- holders/menu/main menu loop the loop.gd
+++ holders/menu/main menu loop the loop.gd
@@ -3,7 +3,7 @@ extends Control
 onready var new_game = preload("res://levels/proto2.tscn")
 
 func _on_newgame_pressed():
-    get_tree().change_scene_to(new_game)
+	get_tree().change_scene_to(new_game)
 
 func _on_instrucoes_pressed():
-    get_tree().change_scene("res://holders/menu/controles.tscn")
+	get_tree().change_scene("res://holders/menu/controles.tscn")
